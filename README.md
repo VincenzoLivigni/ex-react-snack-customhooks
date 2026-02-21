c5ad505e6533413112489084fdac0796439e0524
VincenzoLivigni <enzolivigni13@gmail.com>
2026-02-21 23:30:55 +0100

    Milestone 1

---
+++ README.md
@@ -0,0 +1,6 @@
+Snack 1: useSwitch() – Toggle Booleano
+
+✅ - 1 Creare un custom hook per alternare un valore booleano tra true e false.
+    ✔️ - Inizialmente false.
+    ✔️ - Ritorna il valore attuale e una funzione toggle() per cambiarlo.
+    ✔️ - Quando toggle() viene chiamato, il valore passa da true → false e viceversa.
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import useSwitch from "../hooks/useSwitch";
+
 function App() {
+
+  const { isOn, toggle } = useSwitch();
 
   return (
     <>
+      <h1>Custom Hooks</h1>
 
+      <div>
+        <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+        <button onClick={toggle}>Cambia Stato</button>
+      </div>
     </>
   )
 }
